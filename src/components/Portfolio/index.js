a8e16f2ab32e7a91afcfe16e8a34bbfb0681ce58
ReenaH <Reena_Hunjan@yahoo.com>
2022-09-20 18:08:30 -0400

    contact form

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -10,11 +10,11 @@ import photo4 from "../../assets/small/commercial/4.jpg";
 function Portfolio() {
   //list the projects
   const projects = [
-  {name: "Project1", githublink: "www", deployedLink: "www", pic: photo,},
-  { name: "Project2",githublink: "www", deployedLink: "www", pic: photo1,},
-  { name: "Project3", githublink: "www", deployedLink: "www", pic: photo2,},
-  { name: "Project4", githublink: "www", deployedLink: "www", pic: photo3,},
-   { name: "Project5", githublink: "www", deployedLink: "www", pic: photo4,},
+  {name: "Project1", githublink: "www", deployedLink: "www", pic: photo, desc: 'something',},
+  { name: "Project2",githublink: "www", deployedLink: "www", pic: photo1, desc: 'something',},
+  { name: "Project3", githublink: "www", deployedLink: "www", pic: photo2, desc: 'something',},
+  { name: "Project4", githublink: "www", deployedLink: "www", pic: photo3, desc: 'something',},
+   { name: "Project5", githublink: "www", deployedLink: "www", pic: photo4, desc: 'something',},
 
  ];
 
@@ -32,6 +32,7 @@ function Portfolio() {
           <img
             className="img-thumbnail mx-1"
             src={project.pic}
+            alt={project.desc}
           />
       </div>
 
